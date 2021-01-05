4d3f80b6a9c545835f1539769722f388c03c424b
Naruhiko Nagata <nullhiko@astranox.com>
2021-01-05 21:55:58 +0900

    Data modified

--- main.py
+++ main.py
@@ -5,14 +5,10 @@ from statistics import stdev
 from scipy import stats
 import numpy as np
 import pandas as pd
-import matplotlib
-matplotlib.use('Agg')
 
 csvname = input('csvのファイルネームを入力: ')
 data = pd.read_csv(csvname + '.csv')
 print(data.head())
-#df_beer = data.iloc[:, [1]]
-#df_b_beer = data.iloc[:, [2]]
 
 df1_name = input('対象の列の名前を入力(データ1):')
 df2_name = input('対象の列の名前を入力(データ2):')
@@ -20,23 +16,26 @@ df1 = data.loc[:, df1_name]
 df2 = data.loc[:, df2_name]
 
 class Data:
-    def __init__(self, name, data1, data2):
+    def __init__(self, name, data1, data2, user_p):
         self.name = name
-        self.data1 = data1
-        self.data2 = data2
+        self.data1 = pd.read_csv(name + '.csv').loc[:, data1]
+        self.data2 = pd.read_csv(name + '.csv').loc[:, data2]
+        self.user_p = user_p
+        self.datalist1 = list(self.data1.values.flatten())
+        self.datalist2 = list(self.data2.values.flatten())
 
     @staticmethod
     def ask():
         return Data(
                 input('filename?: '),
-                input('')
+                input('data row name?: '),
+                input('data2 row name?: '),
+                input('pvalue?: ')
                 )
 
-    def listize(self):
-        datalist1 = list(self.data1.values.flatten())
-        datalist2 = list(self.data2.values.flatten())
 
 
-class SdJudge:
-    def is_normal_dist(self, data):
-        if Data.listize.
+class SdJudge(Data):
+    def is_normal_dist(self, datalist1, user_p):
+        result = stats.shapiro(datalist1)
+        judge = bool(result[1] > user_p)
