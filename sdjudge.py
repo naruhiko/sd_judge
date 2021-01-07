465f27fe7819e8acbd67bd8be428297441235796
Naruhiko Nagata <nullhiko@astranox.com>
2021-01-07 14:17:20 +0900

    feat: main func added

--- sdjudge.py
+++ sdjudge.py
@@ -5,6 +5,7 @@ from statistics import stdev
 from scipy import stats
 import numpy as np
 import pandas as pd
+import decimal
 
 
 class SdJudge:
@@ -21,19 +22,25 @@ class SdJudge:
         pvalue = input('pvalue?: ')
         if len(pvalue) == 0:
             pvalue = 0.05
+        else:
+            pvalue = float(pvalue)
+
+        filename = input('filename?: ')
+        print(pd.read_csv(filename + '.csv').head())
+
         while True:
-            is_rel = input('is it relative?[y/n]: ')
-            if is_rel == 'y' or is_rel == 'n':
+            is_rel = input('is it relative?[[y]/n]: ')
+            if is_rel == 'y' or is_rel == 'n' or is_rel == '':
                 break
             else:
                 print('enter correctly, once more.')
         return SdJudge(
-                input('filename?: '),
+                filename,
                 input('data row name?: '),
                 input('data2 row name?: ') or 'none',
                 pvalue,
                 is_rel
-                )
+                ), is_rel
 
 
 
@@ -43,7 +50,7 @@ class SdJudge:
         result_2 = stats.shapiro(self.datalist2)
         judge_2 = bool(result_2[1] > self.user_p)
         judge = bool(judge_1 and judge_2) # if judge_1 and judge_2 is true, judge is true. otherwise False.
-        return judge_1, judge_2, judge
+        return judge_1, judge_2, judge, result_1[1], result_2[1]
 
 
     def variance_bartlett(self):
@@ -72,7 +79,7 @@ class SdJudge:
 
     def ttest_welch(self):
         result = stats.ttest_ind(self.datalist1, self.datalist2, equal_var=False)
-        judge = bool(result[1] > self.user_p)
+        judge = bool(result[1] > self.user_p) 
         return judge, result[1]
 
 
@@ -89,4 +96,28 @@ class SdJudge:
 
 
 if __name__ == '__main__':
-    SdJudge.is_normal_dist(SdJudge.ask())
+    usr = SdJudge.ask()
+    norm_result = SdJudge.is_normal_dist(usr[0])
+    print('data1 is normal distribution: {}, p={} \ndata2 is normal distribution: {}, p={}'.format(norm_result[0], norm_result[3], norm_result[1], norm_result[4]))
+    if norm_result[2] is True:
+        bart_result = SdJudge.variance_bartlett(usr[0])
+        print('Bartlett normal dist. kai2zero : {}, p={}'.format(bart_result[0], bart_result[1]))
+        if  usr[1]== 'y' or usr[1] == '':
+            if bart_result[0] is True:
+                t_ind_result = SdJudge.ttest_student_ind(usr[0])
+                print('t test indivisual student : {}, p={}'.format(t_ind_result[0], t_ind_result[1]))
+            else:
+                t_welch_result = SdJudge.ttest_welch(usr[0])
+                print('t test indivisual welch : {}, p={}'.format(t_welch_result[0], t_welch_result[1]))
+        elif usr[1] == 'n':
+            t_rel_result = SdJudge.ttest_student_rel(usr[0])
+            print('t test relative : {}, p={}'.format(t_rel_result[0], t_rel_result[1]))
+    else:
+        levene_result = SdJudge.variance_levene(usr[0])
+        print('Levene not normal dist. : {}, p={}'.format(levene_result[0], levene_result[1]))
+        if usr[1]== 'y' or usr[1] == '':
+            mann_result = SdJudge.mann(usr[0])
+            print('Mann-whitney not normal dist. : {}, p={}'.format(mann_result[0], mann_result[1]))
+        elif usr[1]== 'n':
+            wilc_result = SdJudge.wilcoxon_signed(usr[0])
+            print('Wilcoxon Signed-rank : {}, p={}'.format(wilc_result[0], wilc_result[1]))
